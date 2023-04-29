563a9dbd4c3aafa7fa6c2c651fc3868174631e27
GarimaM01 <garima.mahto@sakec.ac.in>
2023-04-29 15:37:53 +0530

    controller update 2

--- src/main/java/com/driver/controllers/PaymentController.java
+++ src/main/java/com/driver/controllers/PaymentController.java
@@ -15,29 +15,29 @@ public class PaymentController {
     @PostMapping("/pay")
     public Payment pay(@RequestParam Integer reservationId, @RequestParam Integer amountSent, @RequestParam String mode) throws Exception{
         //Attempt a payment of amountSent for reservationId using the given mode ("cASh", "card", or "upi")
-        Payment payment = paymentService.getPaymentByReservationId(reservationId);
+        //Payment payment = paymentService.getPaymentByReservationId(reservationId);
 
         //If the amountSent is less than bill, throw "Insufficient Amount" exception, otherwise update payment attributes
-        if(payment==null){
-            throw new Exception("Payment not found for this Id: " + reservationId);
-        }
-        Integer bill =payment.getAmount();
-        if(amountSent < bill){
-            throw new Exception("Insufficient Amount");
-        }
+//        if(payment==null){
+//            throw new Exception("Payment not found for this Id: " + reservationId);
+//        }
+//        Integer bill =payment.getAmount();
+//        if(amountSent < bill){
+//            throw new Exception("Insufficient Amount");
+//        }
 
         //If the mode contains a string other than "cash", "card", or "upi" (any character in uppercase or lowercase), throw "Payment mode not detected" exception.
-        if (!(mode.equalsIgnoreCase("cash") || mode.equalsIgnoreCase("card") || mode.equalsIgnoreCase("upi"))){
-            throw  new Exception("Payment mode not detected");
-        }
+//        if (!(mode.equalsIgnoreCase("cash") || mode.equalsIgnoreCase("card") || mode.equalsIgnoreCase("upi"))){
+//            throw  new Exception("Payment mode not detected");
+//        }
 
 
         //Note that the reservationId always exists
-        payment.setAmountPaid(amountSent);
-        payment.setMode(mode);
-        payment.setPaymentStatus(PaymentStatus.COMPLETED);
-        paymentService.updatePayment(payment);
-        return payment;
-        //return  paymentService.pay(reservationId, amountSent, mode);
+//        payment.setAmountPaid(amountSent);
+//        payment.setMode(mode);
+//        payment.setPaymentStatus(PaymentStatus.COMPLETED);
+//        paymentService.updatePayment(payment);
+//        return payment;
+        return  paymentService.pay(reservationId, amountSent, mode);
     }
 }

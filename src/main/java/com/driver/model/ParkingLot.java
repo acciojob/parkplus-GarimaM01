aca6eb021df1bae24e9c31f6f77e97c8eff49b57
GarimaM01 <garima.mahto@sakec.ac.in>
2023-04-29 19:37:07 +0530

    modified parkinglot

--- src/main/java/com/driver/model/ParkingLot.java
+++ src/main/java/com/driver/model/ParkingLot.java
@@ -1,16 +1,13 @@
 package com.driver.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.*;
 
 @Entity
 public class ParkingLot {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String address;
